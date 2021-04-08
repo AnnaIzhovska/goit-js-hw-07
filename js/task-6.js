d32fe7d531417fc50b5bbf031392e15709afa953
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-04-08 12:07:59 +0200

    Задачи

--- js/task-6.js
+++ js/task-6.js
@@ -5,7 +5,7 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur)
 
 function onInputBlur() {
-if(inputEl.value.length === 6){
+if(inputEl.value.length == inputEl.dataset.length){
 inputEl.classList.add('valid');
 inputEl.classList.remove('invalid');
 }
@@ -15,3 +15,18 @@ inputEl.classList.remove('valid');
 }
 console.log('Потерян фокус');
 }
+
+
+// 2 Вариант
+
+// const inputValid = document.querySelector('#validation-input');
+// inputValid.addEventListener('blur', () => {
+//     const inputTextLength = inputValid.value.length;
+//     const maxLength = inputValid.getAttribute('data-length');
+//     if (inputTextLength > maxLength || inputTextLength < maxLength) {
+//      inputValid.classList.add('invalid');
+//     }  else {
+//         inputValid.classList.add('valid')
+//         inputValid.classList.remove('invalid');
+//     }         
+// })
--- js/task-8.js
+++ js/task-8.js
@@ -1,16 +1,37 @@
-const inputEl = document.querySelector('[type = number]');
-const renderBtn = document.querySelector('[data-action = render]');
-const destroyBtn = document.querySelector('[data-action = destroy]');
+const inputEl = document.querySelector('input');
+const btns = document.querySelectorAll('button');
 const boxesEl = document.querySelector('#boxes');
 
-console.log(inputEl);
-console.log(renderBtn);
-console.log(destroyBtn);
-console.log(boxesEl);
-
-// function createBoxes(amount) => {
-
-// }
-
-// function destroyBoxes() => {
-//     }
+const createBoxes = amount => {
+    const boxesArr = [];
+    let dimension = 30;
+    for (let index = 1; index <= amount; index+=1) {
+        const box = document.createElement('div');
+        const red = Math.floor(Math.random() * 250);
+        const green = Math.floor(Math.random() * 250);
+        const blue = Math.floor(Math.random() * 250);
+        const color = `rgb(${red}, ${green}, ${blue})`;
+        box.style.width = `${dimension}px`;
+        box.style.height = `${dimension}px`;
+        box.style.backgroundColor = color;
+        box.style.position = 'relative'
+        box.style.left = ` ${Math.floor(Math.random() * 75)}%`;
+        box.style.top = ` ${Math.floor(Math.random() * 100)}%`;
+        boxesArr.push(box);
+        dimension += 10;
+    }
+    boxesEl.append(...boxesArr)
+    console.log(createBoxes)
+}
+let counterValue = 0;
+btns.forEach(function (btn) {
+     btn.addEventListener('click', function (e) {
+        if (btn.dataset.action === 'render') {
+            createBoxes(inputEl.value);
+        }
+        else if (btn.dataset.action === 'destroy') {
+            boxesEl.innerHTML = '';
+         }
+         inputEl.value = counterValue
+    });
+})
