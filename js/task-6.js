96b16cd26c4fb099e12ab0b28398183d2ded6a3a
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-04-12 16:20:07 +0200

    Gallery

--- js/task-6.js
+++ js/task-6.js
@@ -6,12 +6,12 @@ inputEl.addEventListener('blur', onInputBlur)
 
 function onInputBlur() {
 if(inputEl.value.length == inputEl.dataset.length){
-inputEl.classList.add('valid');
-inputEl.classList.remove('invalid');
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
 }
 else {
-inputEl.classList.add('invalid');
-inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
 }
 console.log('Потерян фокус');
 }
